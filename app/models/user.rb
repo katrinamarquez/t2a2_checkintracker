debf9bf6a11d2775fa05a47706a417a4f859a0cc
katrinamarquez <katrina.l.marquez@gmail.com>
2020-08-05 21:15:32 +1000

    added in roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :sign_ins
   has_many :sign_outs
+
+  # 'Standard' means person clocking in, 'Restaurant' adds ability to create a restaurant and 'Admin' has access to everyone.
+  # 'Admin' will not be allowed to be selected by users. 
+  enum role: [:standard, :restaurant, :admin]
+
+  # Default role for users when they sign on is Standard
+  after_initialize do 
+    if self.new_record?
+      self.role ||= :Standard
+    end
+  end
+  
 end
