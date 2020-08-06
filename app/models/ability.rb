0fa804e18b3c8f2f0afba2af3894f68b209bd3d5
katrinamarquez <katrina.l.marquez@gmail.com>
2020-08-06 22:06:59 +1000

    authenticate before using app and old cancan code

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,22 +4,20 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Restaurant can create restaurants and user details. 
-    if user.role? :restaurant
-      can :manage, Restaurant
-      can :read, SignIn  
-      can :read, Feedback 
-      can :manage, User
-    end 
-
-    # Customer can view restaurant details and view other functions. 
-    if user.role? :customer
-      can :read, Restaurant
-      can :manage, SignIn
-      can :manage, Feedback
-      can :manage, User
-    end
-  end
+    # user ||= User.new
+    # # Customer
+    # if user.has_role? :customer
+    #   can :read, Restaurant 
+    #   can :manage, SignIn
+    #   can :manage, Feedback
+    # # Restaurant
+    # elsif user.has_role? :restaurant 
+    #   can :manage, Restaurant
+    #   can :read, SignIn  
+    #   can :read, Feedback 
+    # else
+    #   can :read, :all
+    # end
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
   end
